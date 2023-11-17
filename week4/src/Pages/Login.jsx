b19b50e02b1883658f3cf22c0c40915ec58c19b0
Jun-min2 <cjm990622@naver.com>
2023-11-18 02:20:32 +0900

    Toast

--- week4/src/Pages/Login.jsx
+++ week4/src/Pages/Login.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { createPortal } from "react-dom";
 
 const Login = () => {
   const [loginUserId, setLoginUserId] = useState("");
   const [loginPassWord, setLoginPassWord] = useState("");
   const navigate = useNavigate();
-
+  const [fail, setFail] = useState("");
+  const [open, setOpen] = useState(false);
   const handleLogin = async () => {
     try {
       const response = await axios.post(
@@ -20,40 +22,47 @@ const Login = () => {
       );
       response && navigate(`/MyPage/${response.data.id}`);
     } catch (e) {
-      console.log(e);
+      setFail(e.response.data.message);
+      setOpen(true);
+      setTimeout(() => setOpen(false), 3000);
     }
   };
 
   return (
-    <Container>
-      <h1>Login</h1>
-      <section>
-        ID
-        <input
-          type="text"
-          placeholder="아이디를 입력해주세요"
-          value={loginUserId}
-          onChange={(e) => setLoginUserId(e.target.value)}
-        />
-      </section>
-      <section>
-        PASSWORD
-        <input
-          type="text"
-          placeholder="비밀번호 입력해주세요"
-          value={loginPassWord}
-          onChange={(e) => setLoginPassWord(e.target.value)}
-        />
-      </section>
+    <>
+      <Container>
+        <h1>Login</h1>
+        <section>
+          ID
+          <input
+            type="text"
+            placeholder="아이디를 입력해주세요"
+            value={loginUserId}
+            onChange={(e) => setLoginUserId(e.target.value)}
+          />
+        </section>
+        <section>
+          PASSWORD
+          <input
+            type="text"
+            placeholder="비밀번호 입력해주세요"
+            value={loginPassWord}
+            onChange={(e) => setLoginPassWord(e.target.value)}
+          />
+        </section>
 
-      <Button type="button" onClick={handleLogin}>
-        로그인{" "}
-      </Button>
+        <Button type="button" onClick={handleLogin}>
+          로그인{" "}
+        </Button>
 
-      <Button type="button" to="/SignUp">
-        회원가입{" "}
-      </Button>
-    </Container>
+        <Button type="button" to="/SignUp">
+          회원가입{" "}
+        </Button>
+      </Container>
+
+      {open &&
+        createPortal(<Toast>{fail}</Toast>, document.getElementById("toast"))}
+    </>
   );
 };
 export default Login;
@@ -63,6 +72,10 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+`;
+const Toast = styled.div`
+  background-color: black;
+  color: white;
 `;
 
 const Button = styled(Link)`
